e3fc5ba91add9c92a72a3e303dd8b6814d213db7
gatchapin <gatchapin@gmx.com>
2018-12-28 12:39:48 +0000

    add copyright

--- pospcase-scheme.el
+++ pospcase-scheme.el
@@ -1,3 +1,26 @@
+;;; pospcase-scheme.el: Pospcase extension for Scheme -*- lexical-binding: t -*-
+
+;; Copyright (C) 2018-2019 gatchapin
+
+;; Author: gatchapin
+;; Keywords: languages, faces
+;; URL: https://github.com/gatchapin2/pospcase-scheme
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Code:
+
 (require 'pospcase-font-lock)
 
 (defun pospcase-match-scheme-define (limit)
